6c38919db90cf92149fd8845d7335a6de774833e
jairus-m <jairusmartinez@gmail.com>
2024-01-15 07:59:20 -0800

    scd

---
+++ snapshots/scd_raw_listings.sql
@@ -0,0 +1,13 @@
+-- snapshot table name
+{% snapshot scd_raw_listings %}
+
+{{
+config(
+       target_schema='DEV',
+       unique_key='id',
+       strategy='timestamp',
+       updated_at='updated_at',
+       invalidate_hard_deletes=True
+) }}
+select * FROM {{ source('airbnb', 'listings') }}
+{% endsnapshot %}
